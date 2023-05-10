7d7c42cb51ddff4f6197ff19a2380b6a80703cc7
amarcal <127268589+amarcal07@users.noreply.github.com>
2023-05-10 20:33:32 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,20 @@ function validarNome(){
     }
 }
 
+function validarEmail() {
+  let txtEmail = document.querySelector("#txtEmail");
+
+  if (email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1) {
+    txtEmail.innerHTML = "E-mail inválido";
+    txtEmail.style.color = "red";
+    emailOk = false;
+  } else {
+    txtEmail.innerHTML = "✔";
+    txtEmail.style.color = "green";
+    emailOk = true;
+  }
+}
+
 function validarEmailRegEx(){
     let regex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/
     let txtEmail = document.querySelector("#txtEmail")
